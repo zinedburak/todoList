c489b5deea1b83a5c2216bb594ebb0bcb0a087e7
zinedburak <zined_burak@hotmail.com>
2021-09-08 20:11:06 +0300

    created tests for button fuctionalaties

--- todolist_frontend/src/components/__test__/Form.test.js
+++ todolist_frontend/src/components/__test__/Form.test.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import {render,fireEvent, queryByPlaceholderText} from '@testing-library/react'
 import Form from "../Form"
 import "@testing-library/jest-dom/extend-expect"
 
 
 it("renders correctly", () => {
-
-    const component = render(<Form />);
+    
+    const component = render(<Form/>)
     const button = component.getByTestId("add-button");
     const input_text = component.getByPlaceholderText("Add Todo");
     expect(button).toBeTruthy;
@@ -18,12 +18,40 @@ it("renders correctly", () => {
 
 describe("Input Value", () => {
     it("updates on change", () => {
-        const component = render(<Form/>)
-
+        const setInputText = jest.fn();
+        const component = render(<Form setInputText={setInputText}/>)
         const todoInput = component.getByPlaceholderText("Add Todo")
 
         fireEvent.change(todoInput, {target: {value: "test"}})
 
         expect(todoInput.value).toBe("test")
+    })
+})
+
+describe("Add Button" , () => {
+    describe("calls function when pressed with no input text" , () => {
+        it("it does not trigger anything" , () => {
+            const setInputText = jest.fn();
+            const component = render(<Form setInputText={setInputText}/>)
+            const todoInput = component.getByPlaceholderText("Add Todo")
+            const addButton = component.getByTestId("add-button")
+
+            fireEvent.click(addButton)
+
+            expect(setInputText).not.toHaveBeenCalled()
+        })
+    })
+    describe("calls function when pressed with input data" ,() => {
+        it("it does trigger anything" , () => {
+            const setInputText = jest.fn();
+            const component = render(<Form setInputText={setInputText}/>)
+            const todoInput = component.getByPlaceholderText("Add Todo")
+            const addButton = component.getByTestId("add-button")
+
+            fireEvent.change(todoInput, {target: {value: "test"}})
+            fireEvent.click(addButton)
+
+            expect(setInputText).toHaveBeenCalled()
+        })
     })
 })
