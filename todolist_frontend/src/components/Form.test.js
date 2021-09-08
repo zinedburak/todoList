025426e428d0185700a3b18035887c02d8ea5fc1
burak deniz <zined_burak@hotmail.com>
2021-09-08 14:46:26 +0300

    added simple test for form component to make sure that form renders correctly and that input boxe can change with evetn

---
+++ todolist_frontend/src/components/Form.test.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {render,fireEvent} from '@testing-library/react'
+import {Form} from "./Form"
+
+it("renders correctly", () => {
+
+    const {queryByTestId, queryByPlaceHolderText} = render(<Form/>)
+
+    expect(queryByTestId("add-button")).toBeTruthy()
+    expect(queryByPlaceHolderText("Add Todo")).toBeTruthy()
+
+})
+
+
+describe("Input Value", () => {
+    it("updates on change", () => {
+        const {queryByPlaceHolderText} = render(<Form/>)
+
+        const todoInput = queryByPlaceHolderText("Add Todo")
+
+        fireEvent.change(todoInput, {target: {value: "test"}})
+
+        expect(todoInput.value).toBe("test")
+    })
+})
