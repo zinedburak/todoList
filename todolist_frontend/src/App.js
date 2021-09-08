af1c79581481c6b8711449273c5b04f042e51ea7
zinedburak <zined_burak@hotmail.com>
2021-09-08 20:10:23 +0300

    Refactored the Form states to make input Text Global

--- todolist_frontend/src/App.js
+++ todolist_frontend/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 import Form from './components/Form';
+
 function App() {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div>
-      <Form/>
+      <Form inputText={inputText} setInputText={setInputText} />
     </div>
     
   );
