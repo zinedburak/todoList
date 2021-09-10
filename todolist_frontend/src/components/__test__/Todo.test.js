c307373fd655bde09d3f09a3b483289917c9dfe4
zinedburak <zined_burak@hotmail.com>
2021-09-10 20:31:38 +0300

    Added Todo components tests and refactored the Todo component

---
+++ todolist_frontend/src/components/__test__/Todo.test.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react"
+import {render,fireEvent, queryByPlaceholderText} from '@testing-library/react'
+import Todo from "../Todo"
+import "@testing-library/jest-dom/extend-expect"
+
+
+it("renders correctly", () => {
+    const todo = {text: "Dummy Test",id: 0}
+    const component = render(<Todo todo={todo} />)
+
+    const todo_item = component.getByTestId("todo")
+    const todo_text = component.getByText(/Dummy Test/)
+    expect(todo_item).toBeTruthy;
+    expect(todo_text).toBeTruthy;
+    
+
+});
