3d940392cda589a58f04c8512e5b97b9db0000a7
zinedburak <zined_burak@hotmail.com>
2021-09-12 22:03:12 +0300

    made changes in app.js and add api call to get_todos so as an useEffect so that every time the page is refreshed we get the latest todos in our list

--- todolist_frontend/src/App.js
+++ todolist_frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList'
@@ -6,6 +6,21 @@ import TodoList from './components/TodoList'
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    
+    const getTodos =   async () =>{
+           const response = await fetch('http://localhost:8000/api/get_todos',{            
+               headers: {'Content-Type': 'application/json'},
+               credentials: 'include',
+           });
+           const content = await response.json();
+           console.log(content)
+           setTodos(content)
+           
+       }
+       getTodos();
+ },[] )
 
 
   return (
