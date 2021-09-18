07b360099d37ea1afc6cc6c02a49d9a49f849c54
zinedburak <zined_burak@hotmail.com>
2021-09-18 20:31:20 +0300

    added database cleanse after the integration test

--- todolist_backend/tests/todo_controller_test.go
+++ todolist_backend/tests/todo_controller_test.go
@@ -4,9 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"math/rand"
 	"net/http/httptest"
@@ -16,7 +13,12 @@ import (
 	"todolist_backend/database"
 	"todolist_backend/models"
 	"todolist_backend/routes"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/stretchr/testify/assert"
 )
+
 // Setting up the application to prevent code duplication
 func SetupApp() *fiber.App {
 	database.Connect()
@@ -27,6 +29,7 @@ func SetupApp() *fiber.App {
 	routes.Setup(app)
 	return app
 }
+
 // Getting the result from DB and parsing it to a string // added to prevent code duplication
 func GetResultFromDB() string {
 	var todos []models.Todos
@@ -44,8 +47,8 @@ func TestGetTodos(t *testing.T) {
 	app := SetupApp()
 
 	// creating a http request to the get_todos
-	req := httptest.NewRequest("GET","/api/get_todos",nil)
-	resp, _ := app.Test(req,100)
+	req := httptest.NewRequest("GET", "/api/get_todos", nil)
+	resp, _ := app.Test(req, 100)
 
 	// Parsing the response to a raw string
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
@@ -55,14 +58,11 @@ func TestGetTodos(t *testing.T) {
 	dbResultString := GetResultFromDB()
 
 	// Comparing two text
-	assert.Equal(t,bodyString,dbResultString,"They are not equal" )
+	assert.Equal(t, bodyString, dbResultString, "They are not equal")
 	// truncating the todos in order to not save test data
 	database.DB.Exec("Truncate todos")
 
-
-
 }
-
 
 //Unit testing the Add todos function
 func TestAddTodos(t *testing.T) {
@@ -77,25 +77,25 @@ func TestAddTodos(t *testing.T) {
 	postBody := map[string]interface{}{
 		"text": text}
 	body, _ := json.Marshal(postBody)
-	req := httptest.NewRequest("POST", "/api/add_todos",  bytes.NewReader(body))
-	req.Header.Set("Content-Type","application/json")
-	_, err := app.Test(req,100)
+	req := httptest.NewRequest("POST", "/api/add_todos", bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	_, err := app.Test(req, 100)
 	dbResult_After_Add := GetResultFromDB()
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 	// comparing the table status before and after adding new todo_object and if they are not equal we pass the test
-	check := dbResult_Before_Add==dbResult_After_Add
+	check := dbResult_Before_Add == dbResult_After_Add
 
-	assert.False(t, check, "They are not equal " )
+	assert.False(t, check, "They are not equal ")
 	// truncating the todos in order to not save test data
 	database.DB.Exec("Truncate todos")
 }
 
 // Integration testing for add todos
 
-func TestIntegrationAddTodos(t *testing.T){
+func TestIntegrationAddTodos(t *testing.T) {
 	app := SetupApp()
 	// Getting todos from database before adding any todo_object
 	dbResultBeforeAdd := GetResultFromDB()
@@ -109,9 +109,9 @@ func TestIntegrationAddTodos(t *testing.T){
 	postBody := map[string]interface{}{
 		"text": text}
 	body, _ := json.Marshal(postBody)
-	req := httptest.NewRequest("POST", "/api/add_todos",  bytes.NewReader(body))
-	req.Header.Set("Content-Type","application/json")
-	_, err := app.Test(req,100)
+	req := httptest.NewRequest("POST", "/api/add_todos", bytes.NewReader(body))
+	req.Header.Set("Content-Type", "application/json")
+	_, err := app.Test(req, 100)
 	// Checking if there is any error
 	if err != nil {
 		fmt.Println(err.Error())
@@ -128,7 +128,7 @@ func TestIntegrationAddTodos(t *testing.T){
 }
 
 // Integration testing for get todos
-func TestIntegrationGetTodos(t *testing.T){
+func TestIntegrationGetTodos(t *testing.T) {
 	// Testing for empty todos table
 	app := SetupApp()
 
@@ -136,17 +136,12 @@ func TestIntegrationGetTodos(t *testing.T){
 	dbEmptyTodos := GetResultFromDB()
 
 	// Getting todos table with our GetTodos API
-	reqGetTodosEmpty := httptest.NewRequest("GET","/api/get_todos",nil)
-	respGetTodosEmpty, _ := app.Test(reqGetTodosEmpty,100)
+	reqGetTodosEmpty := httptest.NewRequest("GET", "/api/get_todos", nil)
+	respGetTodosEmpty, _ := app.Test(reqGetTodosEmpty, 100)
 
 	// Parsing the response to a raw string
 	bodyBytesEmptyTable, _ := ioutil.ReadAll(respGetTodosEmpty.Body)
 	bodyStringEmptyTable := string(bodyBytesEmptyTable)
-
-
-
-
-
 
 	// Testing for todos table with todos in it
 
@@ -158,9 +153,9 @@ func TestIntegrationGetTodos(t *testing.T){
 	postBody := map[string]interface{}{
 		"text": text}
 	body, _ := json.Marshal(postBody)
-	reqAddTodos := httptest.NewRequest("POST", "/api/add_todos",  bytes.NewReader(body))
-	reqAddTodos.Header.Set("Content-Type","application/json")
-	_, err := app.Test(reqAddTodos,100)
+	reqAddTodos := httptest.NewRequest("POST", "/api/add_todos", bytes.NewReader(body))
+	reqAddTodos.Header.Set("Content-Type", "application/json")
+	_, err := app.Test(reqAddTodos, 100)
 
 	if err != nil {
 		fmt.Println(err.Error())
@@ -170,19 +165,16 @@ func TestIntegrationGetTodos(t *testing.T){
 	dbFilledTodos := GetResultFromDB()
 
 	// Getting todos table wit our GetTodos API  after adding a todo_object
-	reqGetTodosFilled := httptest.NewRequest("GET","/api/get_todos",nil)
-	respGetTodosFilled, _ := app.Test(reqGetTodosFilled,100)
+	reqGetTodosFilled := httptest.NewRequest("GET", "/api/get_todos", nil)
+	respGetTodosFilled, _ := app.Test(reqGetTodosFilled, 100)
 
 	// Parsing the response to a raw string
 	bodyBytesFilledTable, _ := ioutil.ReadAll(respGetTodosFilled.Body)
 	bodyStringFilledTable := string(bodyBytesFilledTable)
 
 	// Comparing the results of db status with the api result for both empty and filled database
-	assert.Equal(t, dbFilledTodos,bodyStringFilledTable, "Filled integration test fails")
-	assert.Equal(t, bodyStringEmptyTable,dbEmptyTodos,"Empty Integration Test Fails" )
-
-
-
-
+	assert.Equal(t, dbFilledTodos, bodyStringFilledTable, "Filled integration test fails")
+	assert.Equal(t, bodyStringEmptyTable, dbEmptyTodos, "Empty Integration Test Fails")
+	database.DB.Exec("Truncate todos")
 
 }
