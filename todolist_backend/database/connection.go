4c0a62c11507a7c0b2a6b3e3fcf23e58dd1ce7ce
zinedburak <zined_burak@hotmail.com>
2021-09-23 00:32:01 +0300

    channged connection type from postgres image to heroku postgres

--- todolist_backend/database/connection.go
+++ todolist_backend/database/connection.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=db user=postgres password=postgres dbname=postgres port=5432"
+	dsn := "host=ec2-34-255-134-200.eu-west-1.compute.amazonaws.com user=dusvljbjslskam password=9df38dec15602670e0fc65086493e5d49f7fb80fe8dfc255ddafe317fc5f393e dbname=d1a78jqqnnk9t8 port=5432"
 	connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
